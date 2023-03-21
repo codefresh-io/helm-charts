a95bab97a344729a7bfa0c55b92a7957af71013c
mikhail-klimko <mikhail.klimko@codefresh.io>
2023-03-21 17:17:29 +0300

    feat(cf-common): add versions for named templates

---
+++ scripts/update-tpl-version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Script iterates over cf-common lib chart ".tpl" files and updates named templates versions to match version in `Chart.yaml`
+
+set -eou pipefail
+
+SRCROOT="$(cd "$(dirname "$0")/.." && pwd)"
+
+CHART_PATH="./charts/cf-common/templates"
+CHART_VERSION="v$(yq eval '.version' ./charts/cf-common/Chart.yaml)"
+
+find  -type f -name '*.tpl' -exec sed -i "s/\(v[0-9]\+\.[0-9]\+\.[0-9]\+\)/$CHART_VERSION/g" {} +
