76046e75c0277ae0e97a64d78e9987889a5dd61c
Daniel Maizel <dani98000k@gmail.com>
2025-02-02 18:32:08 +0200

    feat: Extract Credential from 'authorization' header and set it as 'Authorization' header

--- charts/internal-gateway/files/njs/auth.js
+++ charts/internal-gateway/files/njs/auth.js
@@ -2,9 +2,21 @@ function account_name(r) {
     const auth_entity = r.variables["auth_entity"];
     const b64decoded = Buffer.from(auth_entity, 'base64');
     const json = JSON.parse(b64decoded);
-    const account_name = json.authenticatedEntity.activeAccount.name;
+    const account_name = json.activeAccount.name;
 
     return account_name;
 }
 
-export default {account_name};
+function setAuthHeader(r) {
+    let auth = r.headersIn['authorization'];
+    if (auth) {
+        // Look for the pattern: Credential=<value>/...
+        let matches = auth.match(/Credential=([^\/]+)\//);
+        if (matches && matches.length > 1) {
+            return matches[1];
+        }
+    }
+    return "";
+}
+
+export default { account_name, setAuthHeader };
